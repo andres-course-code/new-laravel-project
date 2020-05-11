277f1c092bb8a09be7dcbef5a7f16be10a7e4f62
Samuel <aandres,7xlol@gmail.com>
2020-05-11 15:55:22 -0500

    finished lesson 16 added vote controls

---
+++ resources/assets/js/fontawesome.js
@@ -0,0 +1,7 @@
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import {faCaretUp,faCaretDown,faStar,faCheck} from '@fortawesome/free-solid-svg-icons'
+
+
+
+library.add(fas, faCaretUp,faCaretDown,faStar,faCheck);
